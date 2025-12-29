c36ebe552688e0b5983593dc12c9cd0836fcc6ee
0ahz <shuangoahz@gmail.com>
2025-12-29 09:53:59 +0800

    chore: up

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,html,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
+
 export default {
+  plugins: [tailwindcss()],
   build: {
     outDir: 'docs',
   },
